15e413fb82e54a4640067d2110df662fcf4872d9
Louis Delia <delia.louis@gmail.com>
2022-03-12 21:27:41 -0500

    trying out router

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import {Router, RouterModule,Routes} from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+const routes: Routes = [
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
